0213801ec08aa43eb367cf1f46707db27352a49b
sanfecker <sanfecker@gmail.com>
2020-09-22 02:18:12 +0100

    Initial Commit

---
+++ musicPlayer/musicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicPlayerApp.swift
+//  musicPlayer
+//
+//  Created by Mac User on 22/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct musicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
